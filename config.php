1e4904e40426461abdb827c62944ae2f5622e235
SniperDZ-Pro <sniperdz.sniperdz@gmail.com>
2021-11-30 14:40:53 +0100

    update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'cloud');
+define('DB_SERVER', 'remotemysql.com');
+define('DB_USERNAME', 'GbzLQkS2FS');
+define('DB_PASSWORD', '8WXJe5tYNM');
+define('DB_NAME', 'GbzLQkS2FS');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
